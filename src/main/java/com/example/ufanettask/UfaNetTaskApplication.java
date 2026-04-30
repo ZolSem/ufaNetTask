d9f4a4a35db4ee404d8572a821a559226202cb30
ZolSem <dwzf2aem@gmail.com>
2026-04-30 14:45:35 +0300

    build gradle

--- src/main/java/com/example/ufanettask/UfaNetTaskApplication.java
+++ src/main/java/com/example/ufanettask/UfaNetTaskApplication.java
@@ -1,9 +1,13 @@
 package com.example.ufanettask;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
+@EnableRabbit
 public class UfaNetTaskApplication {
 
     public static void main(String[] args) {
