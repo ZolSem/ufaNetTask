affc4944b01f325cad245b803e4517e5500942aa
ZolSem <dwzf2aem@gmail.com>
2026-05-01 18:20:31 +0300

    6

--- src/main/java/com/example/ufanettask/cache/controller/UserInvoiceController.java
+++ src/main/java/com/example/ufanettask/cache/controller/UserInvoiceController.java
@@ -27,35 +27,19 @@ public class UserInvoiceController {
             @PathVariable Long userId,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
-        log.info("getUserInfo 1");
         UserInvoice user = userInvoiceCacheService
                 .findUserInvoiceById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("User does not exist with id '" + userId + "'"));
-
-        log.info("getUserInfo 2");
         UserInfoResponse response = new UserInfoResponse();
-        log.info("getUserInfo 3: {}", response);
-
         response.setActiveSubscriptionType(user.getSubscriptionType());
-        log.info("getUserInfo 4: {}", response);
-
         List<Invoice> invoices = user.getInvoices().reversed();
-        log.info("getUserInfo 5: {}", invoices);
-
         List<UserInfoResponse.InvoiceDto> invoiceDtos = user.getInvoices().reversed().stream()
                 .map(invoice -> objectMapper.convertValue(invoice, UserInfoResponse.InvoiceDto.class))
                 .toList();
-        log.info("getUserInfo 6: {}", invoiceDtos);
-
         int start = page * size;
-        log.info("getUserInfo 7: {}", start);
         int end = Math.min(start + size, invoices.size());
-        log.info("getUserInfo 8: {}", end);
         response.setInvoices(invoiceDtos.subList(start, end));
-        log.info("getUserInfo 9: {}", response);
 
-        ResponseEntity<UserInfoResponse> res = ResponseEntity.ok(response);
-        log.info("getUserInfo 10: status {} body {}", res.getStatusCode(), res.getBody());
-        return ResponseEntity.ok(res);
+        return ResponseEntity.ok(response);
     }
 }
